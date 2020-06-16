00f01b58f5ace953d5459da16f2af0de186fcc22
Stephanie Castillo <stephaanie.castillo@gmail.com>
2020-06-16 10:36:09 -0300

    Creacion de llas carpetas y las clases correspondientes para lograr dar de alta la coneccion con postman

---
+++ hoteltresvagos/src/main/java/ar/com/ada/api/hoteltresvagos/services/ReporteService.java
@@ -0,0 +1,27 @@
+package ar.com.ada.api.hoteltresvagos.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.hoteltresvagos.repos.*;
+import ar.com.ada.api.hoteltresvagos.entities.reportes.*;
+
+@Service
+public class ReporteService {
+
+    @Autowired
+    ReservaRepository reporteRepo;
+
+    public List<ReporteEstado> getReporteReservasPorEstado() {
+
+        return reporteRepo.getReporteReservasPorEstado();
+    }
+
+    public List<ReportePorHuesped> getReporteReservasPorHuespedes() {
+
+        return reporteRepo.getReporteReservasPorHuespedes();
+    }
+
+}
