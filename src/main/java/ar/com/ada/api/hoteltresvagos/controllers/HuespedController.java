5cc4533d644eed14429e24e2a8c1450f0e0eb5ef
stephaaniie <stephanie.castillo@gmail.com>
2020-07-31 00:12:12 -0300

    Se implemto el controler del huesped

--- src/main/java/ar/com/ada/api/hoteltresvagos/controllers/HuespedController.java
+++ src/main/java/ar/com/ada/api/hoteltresvagos/controllers/HuespedController.java
@@ -5,36 +5,60 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.hoteltresvagos.entities.Huesped;
-import ar.com.ada.api.hoteltresvagos.services.IHuespedService;
+import ar.com.ada.api.hoteltresvagos.services.implemetations.HuespedService;
+import ar.com.ada.api.hoteltresvagos.services.implemetations.ReporteService;
 
 
 @RestController
+@RequestMapping("/huespedes")
 public class HuespedController {
 
     @Autowired
-    IHuespedService huespedService;
+    HuespedService huespedService;
 
-  
-    @GetMapping("/huespedes")
-    public List<Huesped> getHuespedes(@RequestParam(value = "nombre", required = false) String nombre) {
-        return lh;
+    @Autowired
+    ReporteService reporteService;
+
+    @GetMapping()
+    public List<Huesped> findAll(@RequestParam String nombre) {
+        return huespedService.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Huesped findById(@PathVariable int id) {
+        return huespedService.findById(id);
+    }
+
+    @GetMapping("/{id}")
+    public int findByCantReservas(@PathVariable int id) {
+        return reporteService.getCantidadReservas(huespedService.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Huesped> save(@RequestBody Huesped req) {
+        return new ResponseEntity<>(huespedService.save(req), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public Huesped postHuesped(@PathVariable int id, @RequestBody Huesped req) {
+        req.setHuespedId(id);
+        return huespedService.save(req);
+    }
     
-    @GetMapping("/huespedes/{id}")
-    public ResponseEntity<Huesped> getHuespedById(@PathVariable int id) {
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        huespedService.deleteById(id);
+        return "OK";
     }
-
-    @PostMapping("/huespedes")
-    public ResponseEntity<?> postHuesped(@RequestBody Huesped req) {
-    }
-
-    @PutMapping("/huespedes/{id}")
-    public ResponseEntity<?> postHuesped(@PathVariable int id, @RequestBody Huesped req) {
-    }
-    
-
 }
